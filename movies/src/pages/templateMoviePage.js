4c3f78a28fdeb45501e49f9184fd0831c8c6bc5e
Jessica <20101992@mail.wit.ie>
2024-11-04 11:40:54 +0000

    Using react-query to manage the browser-based cache.

--- movies/src/pages/templateMoviePage.js
+++ movies/src/pages/templateMoviePage.js
@@ -3,7 +3,7 @@ import MovieHeader from "../headerMovie";
 import Grid from "@mui/material/Grid2";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-import { getMovieImages } from "../../api/tmdb-api";
+import { getMovieImages } from "../api/tmdb-api";
 
 const TemplateMoviePage = ({ movie, children }) => {
   const [images, setImages] = useState([]);
