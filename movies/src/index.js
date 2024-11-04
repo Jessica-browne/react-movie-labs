d61423a9c3eeebfb52837718e446971f7e97aeaf
Jessica <20101992@mail.wit.ie>
2024-11-04 12:41:00 +0000

    Added new review support.

--- movies/src/index.js
+++ movies/src/index.js
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import FavoriteMoviesPage from "./pages/favouriteMoviespage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,6 +33,7 @@ const App = () => {
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={ <Navigate to="/" /> } />
+            <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
--- movies/src/pages/movieReviewPage.js
+++ movies/src/pages/movieReviewPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import PageTemplate from "../components/templateMovieListPage";
-import MovieReview from "../../components/movieReviews";
+import MovieReview from "../components/movieReviews";
 
 const MovieReviewPage = (props) => {
   let location = useLocation();
