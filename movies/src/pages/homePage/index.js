9424692bd2b1eb757663e03d59c96b988ee0e789
Jessica <20101992@mail.wit.ie>
2024-11-04 12:06:54 +0000

    Updating movie page to use react query

--- movies/src/pages/homePage/index.js
+++ movies/src/pages/homePage/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { getMovies } from "../api/tmdb-api";
-import PageTemplate from '../components/templateMovieListPage';
+import { getMovies } from "../../api/tmdb-api";
+import PageTemplate from '../../components/templateMovieListPage';
 import { useQuery } from 'react-query';
-import Spinner from '../components/spinner';
+import Spinner from '../../components/spinner';
 
 const HomePage = (props) => {
 
